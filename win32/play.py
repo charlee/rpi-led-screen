5a1f0c42e2fd7fadeebe7b1ef805ed50fca1e8b9
Charlee <oda.charlee@gmail.com>
2023-06-18 20:40:53 -0400

    added youtube player

--- win32/play.py
+++ win32/play.py
@@ -1,17 +1,40 @@
+import webview
 import argparse
+from urllib.parse import urlparse, parse_qs
 
+
+def handle_video():
+    pass
 
 
 def main():
-    
-    # parse arguments
-    # the program should take only one positional argument: the video file
-    parser = argparse.ArgumentParser(description='Play a video file.')
-    parser.add_argument('video_file', metavar='video_file', type=str, help='the video file to play')
+    # parse video url
+    parser = argparse.ArgumentParser()
+    parser.add_argument('url', help='YouTube video URL')
     args = parser.parse_args()
-
-    print(args.video_file)
     
+    # parse video id using urlparse
+    url_data = urlparse(args.url)
+    query = parse_qs(url_data.query)
+    video_id = query["v"][0]
+
+    # generate embed url
+    url = 'https://www.youtube.com/embed/{}?autoplay=1&controls=0&rel=0'.format(video_id)
+    
+    # generate a webpage that contains the above url
+    html = '''
+<html>
+<body style="margin:0px;padding:0px;overflow:hidden">
+  <iframe src="{}" frameborder="0" allowfullscreen style="overflow:hidden;height:100%;width:100%" height="100%" width="100%"></iframe>
+  </div>
+</body>
+</html>
+'''.format(url)
+
+    # create window
+    window = webview.create_window('YouTube Player', html=html, width=640, height=480, resizable=False, fullscreen=False)
+    webview.start(handle_video, window)
+
 
 if __name__ == '__main__':
     main()
